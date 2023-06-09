1e60291d41a57178fb207315af5eb4612c910408
Sreejeeshkumar <sreeapp1947@gmail.com>
2023-06-09 17:51:26 +0530

    getElementbyId

---
+++ dom.js
@@ -0,0 +1,12 @@
+
+//console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent='Hello';
+//headerTitle.innerText='Good bye';
+var header=document.getElementById('main-header')
+header.style.borderBottom='solid 3px #000';
+var header=document.getElementById('firsttitle')
+header.style.fontWeight='bold'
+var header=document.getElementById("firsttitle")
+header.style.color="green";
