77d898836ccd7eb14c0d49d967d8f2949d208045
Sreejeeshkumar <sreeapp1947@gmail.com>
2023-06-25 12:05:27 +0530

    add changes

---
+++ query.js
@@ -0,0 +1,16 @@
+//QUERYSELECTOR
+var secondItem=document.querySelector('.list-group-item:nth-child(2)')
+secondItem.style.backgroundColor=('green')
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)')
+thirdItem.textContent=" "
+thirdItem.style.backgroundColor=('red')
+
+//QUERYSELECTORALL
+var even=document.querySelectorAll('li:nth-child(even)')
+for(var i=0; i<even.length-1; i++){
+    even[i].style.color="green";
+}
+var odd=document.querySelectorAll('li:nth-child(odd)')
+for(var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor="green";
+}
