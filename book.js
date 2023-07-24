cf27b99554dcc689b56de1da2a4a0c0b32b93e7a
Sreejeeshkumar <sreeapp1947@gmail.com>
2023-07-24 16:19:35 +0530

    bookapp change 4

--- book.js
+++ book.js
@@ -14,26 +14,57 @@ function submitForm(event) {
   // localStorage.setItem('userPhone', phone);
   // localStorage.setItem('userName', name);
  // Create an object to store user details
- var userDetails = {
+ var storedUserDetailsJSON = localStorage.getItem('userDetails');
+ var userDetails = {};
+ if (storedUserDetailsJSON) {
+  
+  userDetails = JSON.parse(storedUserDetailsJSON);
+}
+userDetails[name] = {
   date: date,
   email: email,
   time: time,
   phone: phone,
-  name: name,
 };
 
-// Convert the userDetails object to a JSON string
-var userDetailsJSON = JSON.stringify(userDetails);
+var updatedUserDetailsJSON = JSON.stringify(userDetails);
 
-// Store the userDetails JSON string in local storage
-localStorage.setItem('userDetails', userDetailsJSON);
- 
+localStorage.setItem('userDetails', updatedUserDetailsJSON);
+document.getElementById('myForm').reset();
+
+displayUserDetails(userDetails);
 
 
-  console.log('Submitted Data:');
-  console.log('Date:', date);
-  console.log('Email:', email);
-  console.log('Time:', time);
-  console.log('Phone:', phone);
-  console.log('Name:', name);
+console.log('Submitted Data:');
+    console.log('Date:', date);
+    console.log('Email:', email);
+    console.log('Time:', time);
+    console.log('Phone:', phone);
+    console.log('Name:', name);
+  
+    // Retrieve and use the updated user details from local storage
+    var updatedUserDetails = JSON.parse(updatedUserDetailsJSON);
+    console.log('Retrieved Data:');
+    console.log('Date:', updatedUserDetails[name].date);
+    console.log('Email:', updatedUserDetails[name].email);
+    console.log('Time:', updatedUserDetails[name].time);
+    console.log('Phone:', updatedUserDetails[name].phone);
+    console.log('Name:', name);
+
+}
+function displayUserDetails(userDetails) {
+  var userDetailsSection = document.getElementById('userDetailsSection');
+  
+  // Get the name of the last added user (newly submitted user)
+  var lastAddedUserName = Object.keys(userDetails).pop();
+
+  // Generate the string for the last added user details
+  var userDetailsString = 'Name: ' + lastAddedUserName + ', ';
+  userDetailsString += 'Date: ' + userDetails[lastAddedUserName].date + ', ';
+  userDetailsString += 'Email: ' + userDetails[lastAddedUserName].email + ', ';
+  userDetailsString += 'Time: ' + userDetails[lastAddedUserName].time + ', ';
+  userDetailsString += 'Phone: ' + userDetails[lastAddedUserName].phone;
+
+  // Update the userDetailsSection with the user details string
+  userDetailsSection.textContent = userDetailsString;
 }
